a193f2c8fdb7b60eec983b6d9cb210b38829fc47
Miguel Fernandez <miguel.fernandez@docplanner.com>
2022-10-28 08:36:19 +0200

    use differents ways to manage data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,71 +1,42 @@
 <template>
   <div class="home">
+    <h2>{{ appTitle }}</h2>
+    <h3>{{ counterData.title }}: </h3>
     <div>
       <button @click="decreaseCounter" class="btn">-</button>
-      <span class="counter">{{ counter }}</span>
+      <span class="counter">{{ counterData.count }}</span>
       <button @click="increaseCounter" class="btn">+</button>
 
+      <div class="edit">
+        <h4>Edit counter title:</h4>
+        <input v-model="counterData.title" type="text"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-  import { ref }from 'vue'
+  import { reactive }from 'vue'
 
-    const counter = ref(0)
+  const appTitle = 'My Amazing Counter App'
+
+ //    const counter = ref(0), 
+ //         counterTitle = ref('My Counter')
+
+    const counterData = reactive({
+      count: 0,
+      title: 'My counter'
+    })
   
     const increaseCounter = () => {
-      counter.value++
+      counterData.count++
     }
 
     const decreaseCounter = () => {
-      counter.value--
+      counterData.count--
     }
 </script>
 
-<!--<script>
-
-import { ref }from 'vue'
-export  default {
-  setup() {
-    const counter = ref(0)
-  
-    const increaseCounter = () => {
-      counter.value++
-    }
-
-    const decreaseCounter = () => {
-      counter.value--
-    }
-
-
-    return {
-      counter,
-      increaseCounter,
-      decreaseCounter
-    }
-  }
-}
-
-</script>-->
-
-<!--
-<script>
-export default {
-  data: () =>  ({
-      counter: 0
-  }),
-  methods: {
-    increaseCounter() {
-      this.counter++
-    },
-    decreaseCounter() {
-      this.counter--
-    }
-  }
-} 
- 
-</script>-->
 
 <style>
   .home {
@@ -75,5 +46,8 @@ export default {
   .btn, .counter {
     font-size: 40px;
     margin: 10px;
+  }
+  .edit {
+    margin-top: 60px;
   }
 </style>
